96ae03e733cbdd15d3be84a2eaf128b18ac2d269
jkangogo <joelkangogo99@gmail.com>
2021-10-13 17:54:17 +0300

    latest update -v2

---
+++ app/src/main/java/com/threemsystems/rentmanager/DataEntry.java
@@ -0,0 +1,205 @@
+package com.threemsystems.rentmanager;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.threemsystems.rentmanager.Holder.MessageActivity;
+
+public class DataEntry extends AppCompatActivity {
+    Button addProperty, newUnit, newTenant, Payment,newInvoice;
+	Button 	journalEntry, expenditure, back, message,BunitServices;
+    SharedPreferences propertypref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_entry);
+
+        addProperty = findViewById(R.id.btnAddProperty);
+        newUnit = findViewById(R.id.btnNewUnits);
+        newTenant = findViewById(R.id.btnNewTenancy);
+        Payment = findViewById(R.id.btnPayment);
+        newInvoice = findViewById(R.id.btnNewInv);
+        journalEntry = findViewById(R.id.btnJournalEntry);
+        expenditure = findViewById(R.id.btnExpenditure);
+        back = findViewById(R.id.back);
+        message=(Button)findViewById(R.id.btnMessage);
+		BunitServices=(Button)findViewById(R.id.UnitServices);
+		
+        propertypref = getSharedPreferences("user_details", MODE_PRIVATE);
+        String idNo=propertypref.getString("idNo","MisingID");
+
+        message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MessageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        addProperty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //check if user level<9
+                propertypref = getSharedPreferences("user_details", MODE_PRIVATE);
+                String userL=propertypref.getString("userlevel","MisingID");
+                int ul=Integer.parseInt(userL);
+                if(ul==9){
+                    Intent intent= new Intent(getApplicationContext(), AddProperty.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(DataEntry.this);
+                    builder.setMessage("You do not have required permission to create new property")
+                            .setCancelable(false)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+            }
+        });
+
+        newUnit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ////
+                propertypref = getSharedPreferences("user_details", MODE_PRIVATE);
+                String userL=propertypref.getString("userlevel","MisingID");
+                int ul=Integer.parseInt(userL);
+                if(ul==9){
+                    Intent intent = new Intent( getApplicationContext(), AddUnit.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    AlertDialog.Builder builder = new AlertDialog.Builder(DataEntry.this);
+                    builder.setMessage("You do not have required permission to create new units")
+                            .setCancelable(false)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+            }
+        });
+
+        newTenant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                propertypref = getSharedPreferences("user_details", MODE_PRIVATE);
+                String userL=propertypref.getString("userlevel","MisingID");
+                int ul=Integer.parseInt(userL);
+                if(ul==9){
+                    Intent intent = new Intent(getApplicationContext(), com.threemsystems.rentmanager.AddTenant.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    AlertDialog.Builder builder = new AlertDialog.Builder(DataEntry.this);
+                    builder.setMessage("You do not have required permission to create new tenant")
+                            .setCancelable(false)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+
+
+
+            }
+        });
+
+        Payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                propertypref = getSharedPreferences("user_details", MODE_PRIVATE);
+                String userL=propertypref.getString("userlevel","MisingID");
+                int ul=Integer.parseInt(userL);
+                if(ul==9){
+                    Intent intent = new Intent(getApplicationContext(), AddPayment.class);
+                    startActivity(intent);
+                    finish();
+
+                }else{
+                    AlertDialog.Builder builder = new AlertDialog.Builder(DataEntry.this);
+                    builder.setMessage("You do not have required permission to create a payment entry")
+                            .setCancelable(false)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+            }
+        });
+
+        newInvoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), NewInvoice.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        journalEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), JounalEntry.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        expenditure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), NewExpenditure.class);
+                startActivity(i);
+                finish();
+            }
+        });
+		 BunitServices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), AddUnitService.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/threemsystems/rentmanager/Holder/propertyListAdapter.java
@@ -0,0 +1,72 @@
+
+package com.threemsystems.rentmanager.Holder;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import android.widget.LinearLayout;
+
+
+public class propertyListAdapter extends BaseAdapter {
+    private Context context;
+	private ArrayList<String> ownerid;
+    private ArrayList<String> pnames;
+	 private ArrayList<String> pdesc;
+	 
+    //private final String[] propertyvalues;
+
+    public propertyListAdapter
+	(Context context, ArrayList<String> ownerid, ArrayList<String> pnames, ArrayList<String> pdesc) {
+        this.context = context;
+        this.ownerid = ownerid;
+		 this.pnames = pnames;
+		  this.pdesc = pdesc;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+         //Creating a linear layout
+        LinearLayout linearLayout = new LinearLayout(context);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+ 
+        
+        //Creating a textview to show the title
+		TextView textViewownerid = new TextView(context);
+        textViewownerid.setText(ownerid.get(position));
+       
+	    TextView textViewnames = new TextView(context);
+        textViewnames.setText(pnames.get(position));
+		
+		TextView textViewdesc = new TextView(context);
+        textViewdesc.setText(pdesc.get(position));
+ 
+       
+        //Adding views to the layout
+        linearLayout.addView(textViewownerid);  
+		linearLayout.addView(textViewnames);  
+		linearLayout.addView(textViewdesc);  		
+ 
+        //Returnint the layout
+        return linearLayout;
+    }
+
+    @Override
+    public int getCount() {
+        //return propertyvalues.length;
+		 return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+}
